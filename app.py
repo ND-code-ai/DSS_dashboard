d68808f3a94e769a78b3cbfcb067cd6b05540536
Nick Dijkhuizen <55748715+ND-code-ai@users.noreply.github.com>
2024-10-13 17:59:42 +0200

    Update streamlit front-end (WIP)

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import streamlit as st
-import pandas as pd
- 
-st.write("""
-Chart
-""")
- 
-df = pd.read_csv("my_data.csv")
-st.line_chart(df)
+import altair as alt
+from st_pages import EVInfastructure, EVSales, emissions, green_energy
+
+
+st.set_page_config(
+    page_title="EV Dashboard",
+    page_icon="🚗",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+col = st.columns((1.5, 6, 2), gap='medium')
+
+
+with st.sidebar:
+    st.title("Electric Vehicle Dashboard")
+    selected_kpi = st.selectbox("Select a KPI", ["Number of charging stations", "EV sales", "Fossil fuel emissions by cars", "Green energy usage"])
+
+if selected_kpi == "Number of charging stations":
+    st.title("EV Infrastructure - Number of Charging Stations")
+    EVInfastructure.main()
+
+elif selected_kpi == "EV sales":
+    st.title("Increasing EV adoption - EV Sales")
+
+elif selected_kpi == "Fossil fuel emissions by cars":
+    st.title("Increasing EV adoption - Fossil fuel emissions by cars")
---
+++ st_pages/EVInfastructure.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+def main():
+   col = st.columns((8, 1), gap='medium')
+   df = pd.read_csv("data/cleaned_NoC_data.csv")
+
+   with col[0]:
+        bubble_chart = alt.Chart(df).mark_circle().encode(
+            x='Recharging Points',
+            y='Power per station (kW)',
+            size=alt.Size('Total Recharging Power Output (kW)', scale=alt.Scale(range=[60, 1300]), legend=None),  
+            color=alt.Color('Country', legend=None), 
+            tooltip=['Country', 'Recharging Points', 'Power per station (kW)', 'Total Recharging Power Output (kW)']
+        ).properties(
+            title='Recharging Points vs. Power per Station by Country',
+            width=850,
+            height=650,
+        ).interactive()
+
+        text_labels = alt.Chart(df).mark_text(
+            align='center',
+            baseline='top',
+            dy=15,
+            color='white'  # Adjust this value to position the text below the bubble
+        ).encode(
+            x='Recharging Points',
+            y='Power per station (kW)',
+            text='Country'
+        )
+
+        # Combine the bubble chart and text labels
+        final_chart = bubble_chart + text_labels
+
+
+        st.altair_chart(final_chart)
+        st.write("<h1 style='font-size: 25px;'>This is a temporary chloreopleth map of Europe, it will be replace with interactive map</h1>", unsafe_allow_html=True)
+        st.image("map.png", use_column_width=True, caption="Electric Vehicle Charging Stations by Country", output_format="PNG")
