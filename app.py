6987ad673ddf157b2bb2a528726e9a3d25f815d7
Nick Dijkhuizen <55748715+ND-code-ai@users.noreply.github.com>
2024-10-13 18:13:07 +0200

    Added home page and refactor app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import altair as alt
-from st_pages import EVInfastructure, EVSales, emissions, green_energy
+from st_pages import EVInfastructure, EVSales, emissions, green_energy, home
 
 
 st.set_page_config(
@@ -15,14 +15,24 @@ col = st.columns((1.5, 6, 2), gap='medium')
 
 with st.sidebar:
     st.title("Electric Vehicle Dashboard")
-    selected_kpi = st.selectbox("Select a KPI", ["Number of charging stations", "EV sales", "Fossil fuel emissions by cars", "Green energy usage"])
+    selected_kpi = st.selectbox("Select a KPI", ["---", "Number of charging stations", "EV sales", "Fossil fuel emissions by cars", "Green energy usage"])
+    
 
+if selected_kpi == "---":
+    home.main()
+    
 if selected_kpi == "Number of charging stations":
     st.title("EV Infrastructure - Number of Charging Stations")
     EVInfastructure.main()
 
 elif selected_kpi == "EV sales":
     st.title("Increasing EV adoption - EV Sales")
+    EVSales.main()
 
 elif selected_kpi == "Fossil fuel emissions by cars":
     st.title("Increasing EV adoption - Fossil fuel emissions by cars")
+    emissions.main()
+
+elif selected_kpi == "Green energy usage":
+    st.title("Increasing EV adoption - Green energy usage")
+    green_energy.main()
