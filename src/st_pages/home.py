415175137a69dcbbc68d47b85bdbd267e0c78938
Nick Dijkhuizen <55748715+ND-code-ai@users.noreply.github.com>
2024-10-15 17:01:08 +0200

    Delete non-related files

--- src/st_pages/home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def main():
-    st.sidebar.success("Select a KPI above.")
-    st.title("Welcome to the Electric Vehicle Dashboard")
-    st.write("This dashboard aims to provide insights into the adoption of electric vehicles in Europe. Use the sidebar to select a KPI.")
