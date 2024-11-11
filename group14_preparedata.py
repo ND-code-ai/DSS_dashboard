4fca48abc6bbad2c42fc8a722cc30492ce16adff
Tony7UU <132312935+Tony7UU@users.noreply.github.com>
2024-11-11 22:20:53 +0100

    preparedata+deletechart

--- group14_preparedata.py
+++ group14_preparedata.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import requests
 from load_db import load_data_to_db
 def get_outlier_indices_IQR_method(data:np.array) -> np.array:
     q1 = np.percentile(data, 25)
@@ -48,8 +49,10 @@ def preprocess_emissions_data(csv_file_path: str) -> pd.DataFrame: #dataframe
     return df
 
 def preprocess_EV_sales(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
     df.replace(0, np.nan, inplace=True)
-    pass
+    
+    return df
 
 def preprocess_EV_infrastructure(df: pd.DataFrame) -> pd.DataFrame:
     pass
@@ -57,6 +60,41 @@ def preprocess_EV_infrastructure(df: pd.DataFrame) -> pd.DataFrame:
 def main():
     all_dataframes = {}
     
+    ### Sales Data
+    url = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/road_eqr_carpda/?format=JSON&lang=en&freq=A&unit=NR&mot_nrg=ELC&geo=EU27_2020&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=IS&geo=LI&geo=NO&geo=CH&geo=UK&geo=BA&geo=ME&geo=MD&geo=GE&geo=AL&geo=TR&geo=UA&geo=XK&geo=BE&time=2012&time=2013&time=2014&time=2015&time=2016&time=2017&time=2018&time=2019&time=2020&time=2021&time=2022&time=2023"
+    response = requests.get(url)
+    data = response.json()
+
+    values = data['value']
+    geo_labels = data['dimension']['geo']['category']['label']
+    geo_indices = data['dimension']['geo']['category']['index']
+    time_labels = data['dimension']['time']['category']['label']
+    time_indices = data['dimension']['time']['category']['index']
+
+    sorted_countries = [geo_labels[code] for code in sorted(geo_indices, key=geo_indices.get)]
+    sorted_times = sorted(time_indices, key=time_indices.get)
+
+    df = pd.DataFrame(index=sorted_countries, columns=sorted_times)
+
+    for index, value in values.items():
+        # Since the index is a single key, we need to map it to the correct country and time
+        country_idx = int(index) // len(time_indices)  # Determinec which country the value belongs to
+        time_idx = int(index) % len(time_indices)      # Determine which time period the value belongs to
+
+        # Get the actual country name and time label
+        country_code = list(geo_indices.keys())[country_idx]
+        country = geo_labels[country_code]  # Use full country name
+        time = time_labels[list(time_indices.keys())[time_idx]]
+
+        # Insert the value into the correct place in the DataFrame
+        df.loc[country, time] = value
+    
+    sales_data = df
+    cleaned_sales_data = preprocess_EV_sales(sales_data)
+    all_dataframes['EV sales'] = cleaned_sales_data
+    ### Sales data
+
+
     emissions_data = preprocess_emissions_data('data/reduced_energyc1.csv')
     all_dataframes['Emissions'] = emissions_data
     noc_data = pd.read_csv('src/data/scraped_NoC_data.csv')
--- src/st_pages/EV_Prices_DE.py
+++ src/st_pages/EV_Prices_DE.py
@@ -15,90 +15,12 @@ def main():
     # Title and Dropdown Menus for Chart Selection
     st.title('Electric Vehicle Data Visualizations')
     
-    # Dropdown for price charts
-    price_chart_option = st.selectbox('Choose a Price vs Attribute Chart', [
-        'Price Distribution',
-        'Price vs Range',
-        'Price vs Fast-Charging Time',
-        'Price vs Acceleration',
-        'Price vs Top Speed',
-        'Price vs Battery Capacity'
-    ])
-    
     # Dropdown for price category charts
     price_category_option = st.selectbox('Choose a Price Category vs Attribute Chart', [
         'Price Categories vs Range',
         'Price Categories vs Efficiency',
         'Price Categories vs Fast-Charging Time'
     ])
-
-# Display the selected general chart
-    
-    #Price Distribution
-    if price_chart_option == 'Price Distribution':
-        st.subheader('Price Distribution of Electric Vehicles')
-        price_hist = alt.Chart(df).mark_bar().encode(
-            alt.X('Price.DE.:Q', bin=alt.Bin(maxbins=30), title='Price (EUR)'),
-            alt.Y('count()', title='Frequency'),
-            tooltip=['Price.DE.', 'count()']
-        ).properties(width=600, height=400)
-
-        mean_price = df['Price.DE.'].mean()
-        median_price = df['Price.DE.'].median()
-        mean_rule = alt.Chart(pd.DataFrame({'Price': [mean_price]})).mark_rule(color='red').encode(x='Price:Q')
-        median_rule = alt.Chart(pd.DataFrame({'Price': [median_price]})).mark_rule(color='black').encode(x='Price:Q')
-
-        st.altair_chart(price_hist + mean_rule + median_rule)
-
-    #Price vs. Range
-    elif price_chart_option == 'Price vs Range':
-        st.subheader('Price vs Range')
-        price_range_scatter = alt.Chart(df).mark_circle(size=60, color='blue').encode(
-            x=alt.X('Range:Q', title='Range (km)'),
-            y=alt.Y('Price.DE.:Q', title='Price (EUR)'),
-            tooltip=['Price.DE.', 'Range']
-        ).properties(width=600, height=400).interactive()
-        st.altair_chart(price_range_scatter)
-
-    #Price vs. Fast-Charing Time
-    elif price_chart_option == 'Price vs Fast-Charging Time':
-        st.subheader('Price vs Fast-Charging Time')
-        charging_scatter = alt.Chart(df).mark_circle(size=60, color='blue').encode(
-            x=alt.X('Price.DE.:Q', title='Price (EUR)'),
-            y=alt.Y('Fast_charge:Q', title='Fast Charging Time (minutes)'),
-            tooltip=['Price.DE.', 'Fast_charge']
-        ).properties(width=600, height=400).interactive()
-        st.altair_chart(charging_scatter)
-
-    #Price vs. Acceleration
-    elif price_chart_option == 'Price vs Acceleration':
-        st.subheader('Price vs Acceleration')
-        acceleration_scatter = alt.Chart(df).mark_circle(size=60, color='blue').encode(
-            x=alt.X('acceleration..0.100.:Q', title='Acceleration (0-100 km/h, seconds)'),
-            y=alt.Y('Price.DE.:Q', title='Price (EUR)'),
-            tooltip=['acceleration..0.100.', 'Price.DE.']
-        ).properties(width=600, height=400).interactive()
-        st.altair_chart(acceleration_scatter)
-
-    #Price vs. Top Speed
-    elif price_chart_option == 'Price vs Top Speed':
-        st.subheader('Price vs Top Speed')
-        top_speed_scatter = alt.Chart(df).mark_circle(size=60, color='blue').encode(
-            x=alt.X('Price.DE.:Q', title='Price (EUR)'),
-            y=alt.Y('Top_speed:Q', title='Top Speed (km/h)'),
-            tooltip=['Price.DE.', 'Top_speed']
-        ).properties(width=600, height=400).interactive()
-        st.altair_chart(top_speed_scatter)
-
-    #Price vs. Battery Capacity
-    elif price_chart_option == 'Price vs Battery Capacity':
-        st.subheader('Price vs Battery Capacity')
-        battery_scatter = alt.Chart(df).mark_circle(size=60, color='blue').encode(
-            x=alt.X('Price.DE.:Q', title='Price (EUR)'),
-            y=alt.Y('Battery:Q', title='Battery Capacity (kWh)'),
-            tooltip=['Price.DE.', 'Battery']
-        ).properties(width=600, height=400).interactive()
-        st.altair_chart(battery_scatter)
 
 # Display the selected price category chart
 
